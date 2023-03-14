be3a9fc2f164e4085811a56f418ad65090d4446f
Biraj <biraj.pub@gmail.com>
2023-03-14 15:01:56 +0530

    fixes

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 set -e
 
-docker rm $(docker ps -qa)
+docker rm $(docker ps -f status=exited -q)
 docker rmi dummy-docker-app-app dummy-docker-app-mongo
+docker system prune -f
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import fs from "node:fs/promises";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
